e910bdf937c0cd68c756575f705fb0f9f163adb0
Julio César Pérez García <jcesarg@me.com>
2024-03-27 13:56:56 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Agencia de Publicidad | LEKET</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/bundle.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">  
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="icon" href="images/logo-32x32.png" sizes="32x32"/>
+    <link rel="icon" href="images/logo-192x192.png" sizes="192x192"/>
+  </head>
+  <body>
+    <!-- Navbar-->
+    <header id="topnav">
+      <div class="container">
+        <div class="logo">
+          <a href="#home">
+            <img src="images/logo_light.png" alt="" class="logo-light">
+            <img src="images/logo_dark.png" alt="" class="logo-dark">
+          </a>
+        </div>
+        <div class="menu-extras">
+          <div class="menu-item">
+            <a class="navbar-toggle">
+              <div class="lines"><span></span><span></span><span></span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div id="navigation" data-onepage="true">
+          <ul class="navigation-menu nav">
+            <li><a href="#home" data-scroll="true">Inicio</a></li>
+            <li><a href="#nosotros" data-scroll="true">Nosotros</a></li>
+            <li><a href="#servicios" data-scroll="true">Servicios</a></li>
+            <li><a href="#instalaciones" data-scroll="true">Instalaciones</a></li>
+            <li><a href="#contacto" data-scroll="true">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    <!-- Home section-->
+    
+    <section id="home">
+      <!-- Home slider-->
+      <div id="home-slider" class="flexslider kenburn">
+        <ul class="slides">
+          <li>
+            <img src="images/bg_01.jpg" alt="">
+            <div class="slide-wrap">
+              <div class="slide-content bold-text">
+                <div class="container">
+                  <!--<h6 class="upper">We develop web applications.</h6>-->
+                  <h2 class="upper">Soluciones en Marketing y Diseño.</h2>
+                  <p class="mt-50"><a href="#nosotros" data-scroll="true" class="btn btn-color btn-scroll"><span><i class="ti-angle-down"></i></span></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <img src="images/bg_02.jpg" alt="">
+            <div class="slide-wrap">
+              <div class="slide-content bold-text">
+                <div class="container">
+                  <h2 class="upper">Estrategias de Comercialización.</h2>
+                  <p class="mt-50"><a href="#nosotros" data-scroll="true" class="btn btn-color btn-scroll"><span><i class="ti-angle-down"></i></span></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <img src="images/bg_03.jpg" alt="">
+            <div class="slide-wrap">
+              <div class="slide-content bold-text">
+                <div class="container">
+                  <h2 class="upper">Marketing Digital.</h2>
+                  <p class="mt-50"><a href="#nosotros" data-scroll="true" class="btn btn-color btn-scroll"><span><i class="ti-angle-down"></i></span></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <!-- end of home slider-->
+      </div>
+    </section>
+    <!-- end of home section-->
+
+    <section id="nosotros">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-md-offset-2">
+            <div class="title center mb-50">
+              <h2>NOSOTROS</h2>
+              <hr>
+              <p>Desarrollamos estrategias para llegar al mercado objetivo de nuestros clientes de manera rapida y contundente.</p>
+            </div>
+          </div>
+          <div class="col-md-6 col-md-offset-3">
+            <div class="text-center">              
+              <img src="images/logo-192x192.png" alt="" class="mt-50">
+            </div>
+          </div>
+        </div>
+        <!-- end of row-->
+      </div>
+      <!-- end of container-->
+    </section>
+
+    <section id="servicios">
+      <div class="container">
+        <div class="title center">
+          <h3>SERVICIOS</h3>
+          <hr>
+        </div>
+        <div class="section-content">
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="icon-box-small outlined mb-25">
+                <div class="text-center">
+                  <img src="images/branding.png" class="rounded mx-auto d-block" alt="">
+                </div>
+                <hr>
+                <h4>Branding</h4>
+                <p>Creamos la personalidad y un mensaje conceptual claro para tu marca.</p>
+              </div>
+              <!-- end of icon box-->
+            </div>
+            <div class="col-sm-4">
+              <div class="icon-box-small outlined mb-25">                
+                <div class="text-center">
+                  <img src="images/identidad-corporativa.png" class="rounded mx-auto d-block" alt="">
+                </div>
+                <hr>
+                <h4>Identidad Corporativa</h4>
+                <p>Diseñamos múltiples elementos gráficos para la personalidad de tu marca.</p>
+              </div>
+              <!-- end of icon box-->
+            </div>
+            <div class="col-sm-4">
+              <div class="icon-box-small outlined mb-25">                
+                <div class="text-center">
+                  <img src="images/spots-audio-y-video.png" class="rounded mx-auto d-block" alt="">
+                </div>
+                <hr>
+                <h4>Spots de Audio y Video</h4>
+                <p>Creamos contenido audiovisual de alta calidad para comercializar de manera más eficiente tus productos o servicios..</p>
+              </div>
+              <!-- end of icon box-->
+            </div>
+            <div class="col-sm-6">
+              <div class="icon-box-small outlined mb-25">                
+                <div class="text-center">
+                  <img src="images/diseno-web.png" class="rounded mx-auto d-block" alt="">
+                </div>
+                <hr>
+                <h4>Diseño Web</h4>
+                <p>Creamos tu sitio web para que la gente conozca tus productos y servicios a través de Internet.</p>
+              </div>
+              <!-- end of icon box-->
+            </div>
+            <div class="col-sm-6">
+              <div class="icon-box-small outlined mb-25">                
+                <div class="text-center">
+                  <img src="images/produccion-publicitaria.png" class="rounded mx-auto d-block" alt="">
+                </div>
+                <hr>
+                <h4>Producción Publicitaria</h4>
+                <p>Imprimimos y producimos cualquier tipo de material gráfico para la promoción de tus productos y servicios.</p>
+              </div>
+              <!-- end of icon box-->
+            </div>
+          </div>
+          <!-- end of row-->
+        </div>
+        <!-- end of section content-->
+      </div>
+      <!-- end of container-->
+    </section>
+
+    <section id="contacto">
+      <div class="container">
+        <div class="title mt-0 pl-15 txt-sm-center txt-xs-center">
+          <h3 class="upper">CONTACTO</h3>
+          <hr>
+        </div>
+        <div class="section-content">
+          <div class="row">
+            <div class="col-md-7">
+              <div class="reserve-form">
+                <form action="procesar_formulario.php" method="post">
+                  <div class="form-group">
+                    <input class="form-control" type="text" id="nombre" name="nombre" placeholder="Nombre">
+                  </div>
+                  <div class="form-group">
+                    <input class="form-control" placeholder="Tu email" type="email" id="email" name="email">
+                  </div>
+                  <div class="form-group">
+                  <input class="form-control" type="number" name="telefono" id="telefono" placeholder="Teléfono">
+                  </div>
+                  <div class="form-group">
+                    <textarea class="form-control" id="comentarios" name="comentarios" rows="4" cols="50" placeholder="Mensaje"></textarea>
+                  </div>
+                  <div class="form-group text-center">
+                    <button type="submit" class="btn btn-color" value="Submit" name='submit'>Enviar</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="col-md-4 col-md-offset-1 ">
+              <div class="row">
+                <div class="col-md-12 col-sm-4">
+                  <div class="icon-box-basic"><i><img src="images/contacto_01.png" class="rounded mx-auto d-block" alt="..."></i>
+                    <h4>Dirección</h4>
+                    <p><a href="https://maps.app.goo.gl/ADN2Vzs4goVJnUcJA" target="_blank">Calle 4 Sur 3748<br>Col. Anzures, C.P. 72538<br>Puebla, Pue.</a></p>
+                  </div>
+                </div>
+                <div class="col-md-12 col-sm-4">
+                  <div class="icon-box-basic"><i><img src="images/contacto_02.png" class="rounded mx-auto d-block" alt="..."></i>
+                    <h4>Teléfono</h4>
+                    <a href="tel:+52 222 211 7550">22 22 11 75 50</a>
+                  </div>
+                </div>
+                <div class="col-md-12 col-sm-4">
+                  <div class="icon-box-basic"><i><img src="images/contacto_03.png" class="rounded mx-auto d-block" alt="..."></i>
+                    <h4>E-mail</h4>
+                    <a href="mailto:contacto@leket.mx">contacto@leket.mx</a>
+                  </div>
+                </div>
+              </div>
+              <!-- end of row-->
+            </div>
+          </div>
+          <!-- end of row-->
+        </div>
+        <!-- end of section content  -->
+      </div>
+      <!-- end of container-->
+    </section>
+
+    <!-- Footer-->  
+    <footer id="footer" class="pb-50 pt-50">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <div class="copy-text">
+              <p>Copyright © <script>document.write(new Date().getFullYear())</script> <a href="https://leket.mx">LEKET</a>. TODOS LOS DERECHOS RESERVADOS.</p>
+            </div>
+          </div>
+        </div>
+        <ul class="social-list text-center style-2">
+          <li><a target="_blank" href="#"><i class="ti-facebook"></i></a>
+          </li>
+          <li><a target="_blank" href="#"><i class="ti-twitter-alt"></i></a>
+          </li>
+          <li><a target="_blank" href="#"><i class="ti-instagram"></i></a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+    <!-- end of footer-->
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bundle.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+  </body>
+</html>
